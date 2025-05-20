0464d4969ccb9f0b4932b9c4686e73c4396fd00a
AngeloLF <angelo.lamure@gmail.com>
2025-05-20 16:03:08 +0200

    big changes

---
+++ compute_loss_history.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+
+path_root = "./results"
+
+dir_savefig = "analyse"
+path_savefig = f"{path_root}/{dir_savefig}"
+if dir_savefig not in os.listdir(path_root) : os.mkdir(path_savefig)
+
+dir_losses = "losses"
+path_losses = f"{path_savefig}/{dir_losses}"
+if dir_losses not in os.listdir(path_savefig) : os.mkdir(path_losses)
+
+folders_loss = ["loss", "loss_mse", "loss_chi2"]
+if folders_loss[0] not in os.listdir(path_losses) : os.mkdir(f"{path_losses}/{folders_loss[0]}")
+
+path_of_npy = f"{path_root}/Spec2vecModels_Results"
+colors = ['g', 'r', 'b', 'k', 'm', 'y', 'darkred', 'darkgreen']
+all_loss = dict()
+
+
+
+for fold in folders_loss:
+
+	for model in os.listdir(path_of_npy):
+
+		for filename in os.listdir(f"{path_of_npy}/{model}/{fold}"):
+
+			name = f"{model}_{filename}"[:-4]
+			file = f"{path_of_npy}/{model}/{fold}/{filename}"
+
+			all_loss[name] = file
+
+	print(f"Loss name for {fold} : {', '.join(list(all_loss.keys()))}")
+	
+	if fold != 'loss':
+
+		plt.figure(figsize=(16, 8))
+
+		for (name, file), color in zip(all_loss.items(), colors):
+
+			train, valid = np.load(file)
+
+			xmin = np.argmin(valid)
+			ymin = np.min(valid)
+
+			plt.plot(train, color=color, linestyle=':')
+			plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
+			plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
+
+		plt.legend()
+		plt.title(f"Evolution of {name} for each models")
+		plt.xlabel(f"Epoch")
+		plt.ylabel(f"Loss")
+		plt.yscale(f"log")
+		plt.savefig(f"./{path_losses}/{fold}.png")
+
+	else:
+
+		for (name, file), color in zip(all_loss.items(), colors):
+
+			plt.figure(figsize=(16, 8))
+
+			train, valid = np.load(file)
+
+			xmin = np.argmin(valid)
+			ymin = np.min(valid)
+
+			plt.plot(train, color=color, linestyle=':')
+			plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
+			plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
+
+			plt.legend()
+			plt.title(f"Evolution of loss for {name}")
+			plt.xlabel(f"Epoch")
+			plt.ylabel(f"Loss")
+			plt.yscale(f"log")
+			plt.savefig(f"./{path_losses}/{fold}/loss_{name}.png")
