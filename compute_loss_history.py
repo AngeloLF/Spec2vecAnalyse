16f2ad49f9d3801488268d8adfe1bed7ff90dddc
AngeloLF <angelo.lamure@gmail.com>
2025-05-22 12:08:43 +0200

    sc

--- compute_loss_history.py
+++ compute_loss_history.py
@@ -1,80 +1,106 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
-
-
-path_root = "./results"
-
-dir_savefig = "analyse"
-path_savefig = f"{path_root}/{dir_savefig}"
-if dir_savefig not in os.listdir(path_root) : os.mkdir(path_savefig)
-
-dir_losses = "losses"
-path_losses = f"{path_savefig}/{dir_losses}"
-if dir_losses not in os.listdir(path_savefig) : os.mkdir(path_losses)
-
-folders_loss = ["loss", "loss_mse", "loss_chi2"]
-if folders_loss[0] not in os.listdir(path_losses) : os.mkdir(f"{path_losses}/{folders_loss[0]}")
-
-path_of_npy = f"{path_root}/Spec2vecModels_Results"
-colors = ['g', 'r', 'b', 'k', 'm', 'y', 'c', 'darkgreen', 'darkred', 'darkblue', 'gray', 'darkmagenta', 'orange', 'darkcyan']
-all_loss = dict()
+import os, sys, shutil
 
 
 
-for fold in folders_loss:
 
-	for model in os.listdir(path_of_npy):
 
-		for filename in os.listdir(f"{path_of_npy}/{model}/{fold}"):
 
-			name = f"{model}_{filename}"[:-4]
-			file = f"{path_of_npy}/{model}/{fold}/{filename}"
+def work_on(lock=''):
 
-			all_loss[name] = file
+	path_of_npy = f"{path_root}/Spec2vecModels_Results"
+	colors = ['g', 'r', 'b', 'k', 'm', 'y', 'c', 'darkgreen', 'darkred', 'darkblue', 'gray', 'darkmagenta', 'orange', 'darkcyan']
+	all_loss = dict()
 
-	print(f"Loss name for {fold} : {', '.join(list(all_loss.keys()))}")
-	
-	if fold != 'loss':
 
-		plt.figure(figsize=(16, 8))
+	for fold in folders_loss:
 
-		for (name, file), color in zip(all_loss.items(), colors):
+		for model in os.listdir(path_of_npy):
 
-			train, valid = np.load(file)
+			for filename in os.listdir(f"{path_of_npy}/{model}/{fold}"):
 
-			xmin = np.argmin(valid)
-			ymin = np.min(valid)
+				name = f"{model}_{filename}"[:-4]
 
-			plt.plot(train, color=color, linestyle=':')
-			plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
-			plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
+				if lock in name:
+					
+					file = f"{path_of_npy}/{model}/{fold}/{filename}"
+					all_loss[name] = file
 
-		plt.legend()
-		plt.title(f"Evolution of {fold} for each models")
-		plt.xlabel(f"Epoch")
-		plt.ylabel(f"Loss")
-		plt.yscale(f"log")
-		plt.savefig(f"./{path_losses}/{fold}.png")
-
-	else:
-
-		for (name, file), color in zip(all_loss.items(), colors):
+		print(f"Loss name for {fold} : {', '.join(list(all_loss.keys()))}")
+		
+		if fold != 'loss':
 
 			plt.figure(figsize=(16, 8))
 
-			train, valid = np.load(file)
+			for (name, file), color in zip(all_loss.items(), colors):
 
-			xmin = np.argmin(valid)
-			ymin = np.min(valid)
+				train, valid = np.load(file)
 
-			plt.plot(train, color=color, linestyle=':')
-			plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
-			plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
+				xmin = np.argmin(valid)
+				ymin = np.min(valid)
+
+				plt.plot(train, color=color, linestyle=':')
+				plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
+				plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
 
 			plt.legend()
-			plt.title(f"Evolution of loss for {name}")
+			plt.title(f"Evolution of {fold} for each models")
 			plt.xlabel(f"Epoch")
 			plt.ylabel(f"Loss")
 			plt.yscale(f"log")
-			plt.savefig(f"./{path_losses}/{fold}/loss_{name}.png")
+			plt.savefig(f"./{path_losses}/{fold}_{lock}.png")
+
+		else:
+
+			for (name, file), color in zip(all_loss.items(), colors):
+
+				plt.figure(figsize=(16, 8))
+
+				train, valid = np.load(file)
+
+				xmin = np.argmin(valid)
+				ymin = np.min(valid)
+
+				plt.plot(train, color=color, linestyle=':')
+				plt.plot(valid, color=color, linestyle='-', label=f"{name} : best at epoch {xmin} = {ymin:.2f}")
+				plt.scatter(xmin, ymin, facecolor=color, edgecolor='k', marker='d')
+
+				plt.legend()
+				plt.title(f"Evolution of loss for {name}")
+				plt.xlabel(f"Epoch")
+				plt.ylabel(f"Loss")
+				plt.yscale(f"log")
+				plt.savefig(f"./{path_losses}/{fold}/loss_{name}_{lock}.png")
+
+
+
+
+if __name__ == "__main__":
+
+	path_root = "./results"
+
+	dir_savefig = "analyse"
+	path_savefig = f"{path_root}/{dir_savefig}"
+	if dir_savefig not in os.listdir(path_root) : os.mkdir(path_savefig)
+
+	dir_losses = "losses"
+	path_losses = f"{path_savefig}/{dir_losses}"
+	if dir_losses in os.listdir(path_savefig) : shutil.rmtree(path_losses)
+	os.makedirs(path_losses, exist_ok=True)
+
+	folders_loss = ["loss", "loss_mse", "loss_chi2"]
+	if folders_loss[0] not in os.listdir(path_losses) : os.mkdir(f"{path_losses}/{folders_loss[0]}")
+
+
+	if len(sys.argv) > 1:
+
+		locks = sys.argv[1].split(",")
+
+		for lock in locks :
+			print(f"Make lock : {lock}")
+			work_on(lock)
+
+	else:
+
+		work_on()
--- recup_score.py
+++ recup_score.py
@@ -10,9 +10,16 @@ os.makedirs(path_resume, exist_ok=True)
 
 import numpy as np
 
-def generate_html_table(colonnes, lignes, y):
+def generate_html_table(colonnes, lignes, text, y):
 
-    if y.shape != (len(lignes), len(colonnes)):
+
+    tds = {
+        "def" : "td",
+        "min" : 'td style="background-color: #99FF33;"',
+        "max" : 'td style="background-color: #FF6666;"',
+    }
+
+    if text.shape != (len(lignes), len(colonnes)):
         raise ValueError("Les dimensions de y ne correspondent pas aux longueurs des listes ligne et colonne.")
 
     html = '<table border="1" style="border-collapse: collapse; text-align: center;">\n'
@@ -22,12 +29,18 @@ def generate_html_table(colonnes, lignes, y):
     for col in colonnes:
         html += f'<th>{col}</th>'
     html += '</tr>\n'
-    
+
+    argmax = np.argmax(y, axis=0)
+    argmin = np.argmin(y, axis=0)
+
     # Lignes de données
     for i, ligne in enumerate(lignes):
         html += f'  <tr><th>{ligne}</th>'
         for j in range(len(colonnes)):
-            html += f'<td>{y[i, j]}</td>'
+            if   i == argmin[j] : td = tds["min"]
+            elif i == argmax[j] : td = tds["max"]
+            else : td = tds["def"]
+            html += f'<{td}>{text[i, j]}</td>'
         html += '</tr>\n'
     
     html += '</table>'
@@ -39,94 +52,84 @@ def generate_html_table(colonnes, lignes, y):
 
 for score in score_type:
 
-	print(f"\nConstruct score {score}")
+    models = os.listdir(f"{path_analyse}/{score}")
+    tests = list()
 
-	models = os.listdir(f"{path_analyse}/{score}")
-	tests = list()
+    for model in models:
+        tests += os.listdir(f"{path_analyse}/{score}/{model}")
 
-	for model in models:
-		tests += os.listdir(f"{path_analyse}/{score}/{model}")
-
-	tests = list(set(tests))
-
-	print(tests)
+    tests = list(set(tests))
 
 
-	# Sorting lists
-	models.sort()
-	tests.sort()
+    # Sorting lists
+    models.sort()
+    tests.sort()
 
 
-	y = np.zeros((2, len(models), len(tests)))
-	e = np.zeros((2, len(models), len(tests)))
-	x = np.zeros((2, len(models), len(tests))).astype(str)
+    y = np.zeros((2, len(models), len(tests)))
+    e = np.zeros((2, len(models), len(tests)))
+    x = np.zeros((2, len(models), len(tests))).astype(str)
 
-	
-	for m, model in enumerate(models):
+    
+    for m, model in enumerate(models):
 
-		for t, test in enumerate(tests):
+        for t, test in enumerate(tests):
 
-			if test in os.listdir(f"{path_analyse}/{score}/{model}"):
+            if test in os.listdir(f"{path_analyse}/{score}/{model}"):
 
-				with open(f"{path_analyse}/{score}/{model}/{test}/resume.txt", "r") as f:
-					data = f.read().split("\n")[:-1]
+                with open(f"{path_analyse}/{score}/{model}/{test}/resume.txt", "r") as f:
+                    data = f.read().split("\n")[:-1]
 
-				for i, line in enumerate(data):
+                for i, line in enumerate(data):
 
-					label, score_i = line.split("=")
-					mean, std = score_i.split("~")
-					y[i, m, t] = mean
-					e[i, m, t] = std
-					x[i, m, t] = f"{mean} ~ {std}"
-
-
-	if score in ["L1"]:
-
-		y *= 100
-		e *= 100
-
-	print(y)
-	print("*")
-	print(e)
-
-	print()
-
-	with open(f"{path_resume}/{score}.html", "w") as f:
-
-		html_codes = [f"<h1>Score {score}</h1>"]
-
-		for i, typeScore in enumerate(["classic", "norma"]):
-
-			html_codes.append(f"<h2>{typeScore}</h2>")
-			html_codes.append(generate_html_table(tests, models, x[i]))
-
-		f.write('\n'.join(html_codes))
+                    label, score_i = line.split("=")
+                    mean, std = score_i.split("~")
+                    y[i, m, t] = mean
+                    e[i, m, t] = std
+                    x[i, m, t] = f"{mean} ~ {std}"
 
 
-	# 		f.write("\n\n\n\\begin{" + "table" + "}[H]\n")
-	# 		f.write("\\center\n")
-	# 		f.write("\\caption{"+ f"{score} with {typeScore} way" + "}\n")
-	# 		f.write("\\label{" + f"{typeScore}" + "}\n")
+    if score in ["L1"]:
 
-	# 		f.write("\\begin{" + "tabular" + "}{" + f"l|{'c'*len(tests)}" + "}\n")
-	# 		f.write("\\hline\n")
+        y *= 100
+        e *= 100
 
-	# 		f.write(f"Model & {'&'.join(tests)} \\\\ \n")
-	# 		f.write("\\hline\n")
+    with open(f"{path_resume}/{score}.html", "w") as f:
 
-	# 		for m, model in enumerate(models):
+        html_codes = [f"<h1>Score {score}</h1>"]
 
-	# 			lineLatex = [f"\\verb|{model}|"]
+        for i, typeScore in enumerate(["classic", "norma"]):
 
-	# 			for t, test in enumerate(tests):			
+            html_codes.append(f"<h2>{typeScore}</h2>")
+            html_codes.append(generate_html_table(tests, models, x[i], y[i]))
 
-	# 				if e[i, m, t] != 0 : lineLatex.append(f"${y[i, m, t]} \\pm {e[i, m, t]}$")
-	# 				else : lineLatex.append(f" ")
+        f.write('\n'.join(html_codes))
 
-	# 			f.write(' & '.join(lineLatex) + "\\\\ \n")
 
-	# 		f.write("\\end{" + "tabular" + "}\n")
-	# 		f.write("\\end{" + "table" + "}\n")
+    #         f.write("\n\n\n\\begin{" + "table" + "}[H]\n")
+    #         f.write("\\center\n")
+    #         f.write("\\caption{"+ f"{score} with {typeScore} way" + "}\n")
+    #         f.write("\\label{" + f"{typeScore}" + "}\n")
+
+    #         f.write("\\begin{" + "tabular" + "}{" + f"l|{'c'*len(tests)}" + "}\n")
+    #         f.write("\\hline\n")
+
+    #         f.write(f"Model & {'&'.join(tests)} \\\\ \n")
+    #         f.write("\\hline\n")
+
+    #         for m, model in enumerate(models):
+
+    #             lineLatex = [f"\\verb|{model}|"]
+
+    #             for t, test in enumerate(tests):            
+
+    #                 if e[i, m, t] != 0 : lineLatex.append(f"${y[i, m, t]} \\pm {e[i, m, t]}$")
+    #                 else : lineLatex.append(f" ")
+
+    #             f.write(' & '.join(lineLatex) + "\\\\ \n")
+
+    #         f.write("\\end{" + "tabular" + "}\n")
+    #         f.write("\\end{" + "table" + "}\n")
 
 
 
