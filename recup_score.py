30b39bde6975f5d36401d2ad3350bfa643e883ad
AngeloLF <angelo.lamure@gmail.com>
2025-06-05 10:50:07 +0200

    sc

--- recup_score.py
+++ recup_score.py
@@ -136,7 +136,7 @@ def make_score(name_tests, tests, models, score_type, pbar):
             tot_mean = [list(), list()]
             tot_std = [list(), list()]
 
-            for t, test in enumerate(tests):
+            for t, otest in enumerate(tests):
 
                 test = otest if "no0" not in model else f"{test}no0"
 
