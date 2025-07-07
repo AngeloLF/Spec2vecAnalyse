f56606e29dff319a373bf45b28bc68099e0fd9e9
AngeloLF <angelo.lamure@gmail.com>
2025-07-07 13:43:51 +0200

    sc

--- recup_score.py
+++ recup_score.py
@@ -174,7 +174,7 @@ def makePlotAnalyse(ana, score, idec=0.1):
 
         for i in range(len(ns)):
             means_score = [np.mean(s) for s in score_models[i].values()]
-            stds_score = [np.sum(np.array(s)**2)**0.5 for s in score_models_std[i].values()]
+            stds_score = [np.sum(np.array(s)**2)**0.5/3 for s in score_models_std[i].values()]
             mean_score_models[i] = np.min(means_score)
             stds_score_models[i] = stds_score[np.argmin(means_score)]
 
