d4d2e606c803a37ed5fdf63abf414bf64a647c3b
AngeloLF <angelo.lamure@gmail.com>
2025-07-09 13:43:04 +0200

    sc

--- recup_score.py
+++ recup_score.py
@@ -232,7 +232,13 @@ def makePlotAnalyse(ana, score, idec=0.1):
 
 
         # Tab figure
-        df = pd.DataFrame(ana.k2t[k]["tab"], index=ana.k2t[k]["y"], columns=[lab.replace("_", "") for lab in ana.k2t[k]["x"]])
+
+        df_def = pd.DataFrame(ana.k2t[k]["tab"], index=ana.k2t[k]["y"], columns=[lab.replace("_", "") for lab in ana.k2t[k]["x"]])
+        row_vide = pd.DataFrame([[np.nan]*df.shape[1]],             columns=df.columns, index=[''])
+        row_mean = pd.DataFrame(np.mean(ana.k2t[k]["tab"], axis=0), columns=df.columns, index=['Mean'])
+        row_min  = pd.DataFrame(np.min(ana.k2t[k]["tab"], axis=0),  columns=df.columns, index=['Min'])
+
+        df = pd.concat([df_def, row_vide, row_mean, row_min])
 
         if score == "L1" : vmax = min(np.nanmax(ana.k2t[k]["tab"]), 3.0)
         elif score == "chi2" : vmax = min(np.nanmax(ana.k2t[k]["tab"]), 2.0)
