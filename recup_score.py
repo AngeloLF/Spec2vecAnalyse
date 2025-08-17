10620fc024606bddf5ac4a784ac1070666a9c7d3
AngeloLF <angelo.lamure@gmail.com>
2025-08-18 00:12:16 +0200

    sc

--- recup_score.py
+++ recup_score.py
@@ -18,6 +18,10 @@ def recup_mt(score, mode="dispo"):
 
         models = [m for m in os.listdir(f"./results/analyse/{score}") if not "." in m]
 
+        setA = possibility(models=["SCaM"], losses=["chi2", "L1N", "MSE"], trains=["train2k", "train4k", "train8k", "train16k"], lrs=["1e-03", "1e-04", "5e-05", "1e-05", "5e-06", "1e-06"])
+        setB = possibility(models=["SCaM", "SCaMv2", "SotSu", "SotSuv2", "CaTS", "CaTSv2"], losses=["chi2"], trains=["train16k"], lrs=["1e-04", "5e-05", "1e-05", "5e-06", "1e-06"])
+
+        models = list(set(setA+setB))
 
 
     elif mode == "all":
