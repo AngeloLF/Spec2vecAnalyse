8e773bb67e5d2f2027cc941ce7bf02a42576e305
AngeloLF <angelo.lamure@gmail.com>
2025-12-09 11:43:22 +0100

    first update from mI

--- apply_spectractor_old.py
+++ apply_spectractor_old.py
@@ -5,91 +5,53 @@ import numpy as np
 from scipy import interpolate
 from time import time
 import coloralf as c
-import astropy.coordinates as AC
-from astropy import units as u
-import traceback
 
-
-spectractor_version = "Spectractor" 
-for argv in sys.argv:
-    if "=" in argv and argv.split("=")[0] == "specver":
-        spectractor_version = argv.split("=")[1]
-sys.path.append(f"./{spectractor_version}")
+sys.path.append("./SpectractorStage")
 from spectractor.extractor import extractor
 from spectractor.extractor.spectrum import Spectrum
 from spectractor import parameters
-from spectractor.simulation.adr import hadec2zdpar
 
 
 
-
-def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=1, specver="Spectractor", flipy=False):
+def openTest(testname, pathtest="./results/output_simu", varfile="variable_params.pck", hpjson="hparams.json", histjson="hist_params.json", config = "./Spectractor/config/ctio.ini", 
+            makeonly=None, maxloop=1):
 
     testdir = f"{pathtest}/{testname}"
     pred = f"pred_Spectractor_x_x_0e+00"
 
-    # on flip ?
-    if flipy : print(f"{c.g}INFO : Flipy True{c.d}")
-    else : print(f"{c.y}INFO : Flipy False{c.d}")
-
-    # Importation data
-    with open(f"{testdir}/hparams.json", "r") as fjson:
+    with open(f"{testdir}/{hpjson}", "r") as fjson:
         hp = json.load(fjson)
-    vp = np.load(f"{testdir}/vparams.npz")
 
-    # 
+    with open(f"{testdir}/{histjson}", "r") as fjson:
+        hs = json.load(fjson)
+
+    with open(f"{testdir}/{varfile}", "rb") as fpck:
+        vp = pickle.load(fpck)
+
     images = np.sort([f"{testdir}/image/{fimage}" for fimage in os.listdir(f"{testdir}/image")])
-    gain, ron = hp["CCD_GAIN"], hp["cparams"]["CCD_READ_OUT_NOISE"]
 
-    # get config file
-    if f"{hp['telescope'].lower()}.ini" in os.listdir(f"./{specver}/config/") : configName = hp['telescope'].lower()
-    elif "TEL_NAME" in hp.keys() : configName = hp["TEL_NAME"]
-    else : configName = hp['telescope'].lower()
-    config = f"./{specver}/config/{configName}.ini"
-    print(f"Loading config : ", config)
+    gain, ron = hp["CCD_GAIN"], hs["CCD_READ_OUT_NOISE"]
     
     header = fits.Header()
     header["DISPERSER"] = hp["DISPERSER"]
-    header["EXPTIME"] = hp["cparams"]["EXPOSURE"]
-    header['OUTTEMP'] = hp["cparams"]["ATM_TEMPERATURE"]
+    header["EXPTIME"] = hs["EXPOSURE"]
+    header['OUTTEMP'] = hs["ATM_TEMPERATURE"]
     header['OUTPRESS'] = hp["OBS_PRESSURE"]
-    header['OUTHUM'] = hp["cparams"]["ATM_HUMIDITY"]
+    header['OUTHUM'] = hs["ATM_HUMIDITY"]
     header['XLENGTH'] = hp["SIM_NX"]
     header['YLENGTH'] = hp["SIM_NX"]
     header['XPIXSIZE'] = hp["CCD_PIXEL2ARCSEC"]
     header['GTGAIN11'], header['GTGAIN12'], header['GTGAIN21'], header['GTGAIN22'] = gain, gain, gain, gain
     header['GTRON11'], header['GTRON12'], header['GTRON21'], header['GTRON22'] = ron, ron, ron, ron
 
-    rebin = hp["CCD_REBIN"]
-
-    header["FILTER"] = "empty"
     header["FILTERS"] = f"dia {hp['DISPERSER']}"
     header["FILTER1"] = "dia"
     header['FILTER2'] = hp["DISPERSER"]
 
-    header["GRATING"] = hp["DISPERSER"]
-    header["BUNIT"] = "adu"
-    header["OBS-ELEV"] = 2662.99616375123
-    header["OBS-LAT"] =  -30.2446389756252
-    header["LINSPOS"] = hp["DISTANCE2CCD"] - 115
-    if hp["DISPERSER"] == "holo4_003":
-        header ["LINSPOS"] -= 4
-    print(f"Distance CCD in hparams :",  hp["DISTANCE2CCD"])
-
     header["DATE-OBS"] = None
-    header["DATE"] = None
     header['RA'] = "12:53:8.10"
     header['DEC'] = "-18:33:27.78"
     header['HA'] = "01:52:42.92"
-
-    lat = AC.Latitude(hp["OBS_LATITUDE"], unit=u.deg)
-    dec = hp["cparams"]["ADR_DEC"]
-    ha  = hp["cparams"]["ADR_HOUR_ANGLE"]
-    _, par_angle = hadec2zdpar(ha, dec, lat.degree, deg=True)
-
-    header["ROTPA"] = 270 - hp["OBS_CAMERA_ROTATION"] # hp["OBS_CAMERA_ROTATION"] - hp["OBS_CAMERA_RA_FLIP_SIGN"] * par_angle
-
-    print(f"{c.m}ROTPA et par_angle{c.d} : ", header["ROTPA"], par_angle)
 
     xt = np.arange(hp["LAMBDA_MIN"], hp["LAMBDA_MAX"], hp["LAMBDA_STEP"])
 
@@ -97,29 +59,23 @@ def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=
         if fold in os.listdir(testdir) : shutil.rmtree(f"{testdir}/{fold}")
         os.mkdir(f"{testdir}/{fold}")
 
-    times = np.zeros(hp["nsimu"])
+    times = np.zeros(hs["nb_simu"])
 
-    for n in range(hp["nsimu"]):
+    for n in range(hs["nb_simu"]):
 
         if makeonly is None or int(makeonly) == n:
 
             header["TARGET"] = vp["TARGET"][n]
             header["AIRMASS"] = vp["ATM_AIRMASS"][n]
-            header["ANGLE"] = vp["ROTATION_ANGLE"][n]
-
-            header["AMSTART"] = header["AIRMASS"]
-            header["AMEND"] = header["AIRMASS"]
-            header["OBJECT"] = header["TARGET"]
-
-            if flipy : header["ANGLE"] *= -1
+            header["ANGLE"] = - vp["ROTATION_ANGLE"][n]
 
             print(f"\nMake {n}, with angle {header['ANGLE']}")
             
-            data = np.zeros((hp["SIM_NX"], hp["SIM_NX"])) + hp["cparams"]["BACKGROUND_LEVEL"] * hp["cparams"]["EXPOSURE"]
+            data = np.zeros((hp["SIM_NX"], hp["SIM_NX"])) + hs["BACKGROUND_LEVEL"] * hs["EXPOSURE"]
 
             d = np.copy(data).astype(np.float32) * gain
             noisy = np.random.poisson(d).astype(np.float32)
-            noisy += np.random.normal(scale=hp["cparams"]["CCD_READ_OUT_NOISE"]*np.ones_like(noisy)).astype(np.float32)
+            noisy += np.random.normal(scale=hs["CCD_READ_OUT_NOISE"]*np.ones_like(noisy)).astype(np.float32)
 
             data = noisy / gain
             min_noz = np.min(data[data > 0])
@@ -128,23 +84,11 @@ def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=
             bande = int((hp["SIM_NX"] - hp["SIM_NY"])/2)
 
             if makeonly is None: 
-                if flipy : data[bande:bande+hp["SIM_NY"], :] = np.load(images[n]) # [::-1, :]
-                else : data[bande:bande+hp["SIM_NY"], :] = np.load(images[n])
+                data[bande:bande+hp["SIM_NY"], :] = np.load(images[n])[::-1, :]
                 true_name = images[n].split("_")[-1][:-4]
             else: 
-                if flipy : data[bande:bande+hp["SIM_NY"], :] = np.load(f"{testdir}/image/image_{makeonly}.npy") #[::-1, :]
-                else : data[bande:bande+hp["SIM_NY"], :] = np.load(f"{testdir}/image/image_{makeonly}.npy")
+                data[bande:bande+hp["SIM_NY"], :] = np.load(f"{testdir}/image/image_{makeonly}.npy")[::-1, :]
                 true_name = makeonly
-
-            # Reglage AuxTel
-            if hp["telescope"].lower() == "auxtel":
-
-                datap = np.zeros((hp["SIM_NX"]*2, hp["SIM_NX"]*2))
-
-                data = data.T[::-1, ::-1]
-                datap[::2, ::2] = data
-                data = datap
-                rebin *= 2
 
             hdu = fits.PrimaryHDU(data=data.astype(np.float32), header=header)
             hdul = fits.HDUList([hdu])
@@ -171,7 +115,7 @@ def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=
                 loop += 1
 
                 try:
-                    spectrum = extractor.Spectractor(savefile, f"{testdir}/spectrum_fits", guess=[64*rebin, 512*rebin], target_label=vp["TARGET"][n], disperser_label=hp["DISPERSER"], config=config)
+                    spectrum = extractor.Spectractor(savefile, f"{testdir}/spectrum_fits", guess=[64, 512], target_label=vp["TARGET"][n], disperser_label=hp["DISPERSER"], config=config)
                     spectrum.convert_from_flam_to_ADUrate()
                     xp = spectrum.lambdas
                     yp = spectrum.data * gain * spectrum.expo
@@ -183,9 +127,7 @@ def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=
                     ypi = finterp(xt)
                     spectractor_ok = True
 
-                except Exception as e:
-                    print(f"{c.lk}{traceback.format_exc()}{c.d}")
-                    print(f"Exception : {e}")
+                except:
                     print(f"{c.lr}WARNING : loop {loop}/{maxloop} extractor failed ...{c.d}")
                     if loop == maxloop:
                         spectractor_ok = True
@@ -215,6 +157,8 @@ def openTest(testname, pathtest="./results/output_simu", makeonly=None, maxloop=
 
 def viewResult(test_folder, name):
 
+    print(parameters.FLAM_TO_ADURATE)
+
     xt = np.arange(300, 1100)
     yt = np.load(f"./results/output_simu/{test_folder}/spectrum/spectrum_{name}.npy")
     yp = np.load(f"./results/output_simu/{test_folder}/pred_Spectractor_x_x_0e+00/spectrum_{name}.npy")
@@ -233,13 +177,11 @@ if __name__ == "__main__":
     makeonly = None
     test_folder = sys.argv[1]
 
-    #flipy = False if "noflip" in sys.argv else True
-
     for arg in sys.argv[1:]:
 
         if arg[:9] == "makeonly=" : makeonly = arg[9:]
 
 
-    if not "view" in sys.argv : openTest(test_folder, makeonly=makeonly, specver=spectractor_version)
+    if not "view" in sys.argv : openTest(test_folder, makeonly=makeonly)
     else : viewResult(test_folder, makeonly)
 
